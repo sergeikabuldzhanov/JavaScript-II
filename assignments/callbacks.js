168649777b551d9f95f7be7c842c37549661b8b9
sergeikabuldzhanov <thesiegebot@gmail.com>
2019-10-15 17:00:00 +0500

    Completed all the goals and stretch goals.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -38,27 +38,50 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   console.log(test2); // "this Pencil is worth a million dollars!"
 */
 
+console.log('\n CALLBACK CHALLENGES! \n\n');
+
+//Test function
+function testWow(item){
+  console.log(`Wow, it's a ${item}!`);
+}
+//
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
+getLength(items, testWow);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr[arr.length-1]);
 }
+last(items, testWow);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x+y);
 }
+sumNums(1, 2, testWow);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x*y);
 }
+multiplyNums(1, 2, testWow);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for(let i = 0; i<list.length;i++){
+    if(list[i]==item){
+      cb(true)
+    }else{
+      cb(false);
+    }
+  }
 }
+
 
 /* STRETCH PROBLEM */
 
@@ -66,4 +89,14 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  let duplicateFree = [];
+  for(let i = 0; i<array.length; i++){
+    if(!duplicateFree.includes(array[i])){
+      duplicateFree.push(array[i]);
+    }
+  }
+  cb(duplicateFree);
 }
+const itemsDuplicates = ['Pencil', 'Notebook','Pencil', 'Notebook','Pencil', 'Notebook', 'yo-yo', 'Gum'];
+
+removeDuplicates(itemsDuplicates, testWow);
